b9970652207e5a8942f2b6583bbb785ad9d635cb
c.shohom@gmail.com <c.shohom@gmail.com>
2022-03-31 21:46:49 -0400

    Fix GH error

--- gh-pages.js
+++ gh-pages.js
@@ -3,7 +3,7 @@ const ghpages = require('gh-pages');
 ghpages.publish(
     'public', // path to public directory
     {
-        branch: 'gh-pages',
+        branch: 'main',
         repo: 'https://github.com/anInsignificantHuman/hivetrack.git', 
         user: {
             name: 'anInsignificantHuman'
